f5a59ff761854cf53a87763f74d14e2e2d53fc8f
Enes Gülcü <enes.gulcu@hotmail.com>
2022-05-08 13:14:17 +0300

    redux is defined and settings are configured.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { store } from '../redux/store.js';
+import { Provider } from 'react-redux';
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
