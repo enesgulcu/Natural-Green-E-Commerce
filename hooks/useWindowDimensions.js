94da16e658326cc95ec92e8d2b2a3375d298be45
Enes Gülcü <enes.gulcu@hotmail.com>
2022-05-15 22:18:53 +0300

    some change thing not important

--- hooks/useWindowDimensions.js
+++ hooks/useWindowDimensions.js
@@ -1,31 +1,25 @@
-import { useState, useEffect } from 'react';
-
-export default function useWindowDimensions() {
-  const hasWindow = typeof window !== 'undefined';
-
-  function getWindowDimensions() {
-    const width = hasWindow ? window.innerWidth : null;
-    const height = hasWindow ? window.innerHeight : null;
-    return {
-      width,
-      height,
-    };
-  }
-
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
-
-  useEffect(() => {
-    if (hasWindow) {
-      function handleResize() {
-        setWindowDimensions(getWindowDimensions());
-      }
-
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
-    }
-  }, [hasWindow]);
-
-  return windowDimensions;
-}
+/**
+ * // useWindowDimension.ts
+ * * This hook returns the viewport/window height and width
+ * * * https://dev.to/adrien/creating-a-custom-react-hook-to-get-the-window-s-dimensions-in-next-js-135k
+ */
+ import { useEffect, useState } from 'react';
+ const useWindowDimensions = () => {
+     const [windowDimensions, setWindowDimensions] = useState({
+         width: undefined,
+         height: undefined,
+     });
+     useEffect(() => {
+         function handleResize() {
+             setWindowDimensions({
+                 width: window.innerWidth,
+                 height: window.innerHeight,
+             });
+         }
+         handleResize();
+         window.addEventListener('resize', handleResize);
+         return () => window.removeEventListener('resize', handleResize);
+     }, []); // Empty array ensures that effect is only run on mount
+     return windowDimensions;
+ };
+ export default useWindowDimensions;
